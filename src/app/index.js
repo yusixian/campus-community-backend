58783a89d231a0dd5feea599a5c3159623ffe98d
KamenRider41 <229335832@qq.com>
2022-02-20 22:26:09 +0800

    docs(开发记录文档): 自动更新路由use编写文档更新

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@
  * @Author: 41
  * @Date: 2022-02-15 17:29:53
  * @LastEditors: 41
- * @LastEditTime: 2022-02-20 21:51:51
+ * @LastEditTime: 2022-02-20 22:23:51
  * @Description: 
  */
 // 导入包
@@ -19,20 +19,10 @@ render(app, {
   root: path.join(path.resolve(__dirname, '..'), 'views'), //视图的位置
   extname: '.html'// 模板文件的后缀
 })
-// console.log(path.resolve(__dirname, '..'));
 // 导入封装好的路由
 const router = require('../routers/index')
-// const userRouter = require('../routers/user.route')
-// const homeRouter = require('../routers/home.route')
-// const articleRouter = require('../routers/article.route')
-// const partition = require('../routers/partition.route')
-// 创建中间件
 app.use(KoaBody())
-app.use(router.routes())
-// app.use(userRouter.routes())
-// app.use(homeRouter.routes())
-// app.use(articleRouter.routes())
-// app.use(partition.routes()) // 分区的路由
+app.use(router.routes()).use(router.allowedMethods())
 app.use(serve(path.resolve(__dirname, '..') + '/public'))
 // 统一的错误处理
 app.on('error', errHandler)
