034b4b7074f78c85c2398e03dd1da0dff0d0d0ab
cos <3450414733@qq.com>
2022-02-26 20:10:03 +0800

    feat(articles/uploads): 增加文章图片上传接口

--- src/routers/article.route.js
+++ src/routers/article.route.js
@@ -1,14 +1,14 @@
 /*
  * @Author: cos
  * @Date: 2022-02-18 14:09:32
- * @LastEditTime: 2022-02-25 12:36:34
+ * @LastEditTime: 2022-02-26 19:03:24
  * @LastEditors: cos
  * @Description: 文章管理相关路由
  * @FilePath: \campus-community-backend\src\routers\article.route.js
  */
 const Router = require('koa-router')
 
-const { postArticle, deleteArticle, updateArticle, shieldArticle, restoreArticle, getAllArticle, getByID, getByPages, countByFilter } = require('../controller/article.controller')
+const { postArticle, deleteArticle, updateArticle, shieldArticle, restoreArticle, getAllArticle, getByID, getByPages, countByFilter, upload } = require('../controller/article.controller')
 const { articleInfoValidate, articleIDValidate, articleExistValidate, articleFilterValidate } = require('../middleware/article.middleware')
 
 const { auth } = require('../middleware/auth.middleware')
@@ -17,6 +17,7 @@ const { verifyAdmin } = require('../middleware/user.middleware')
 const router = new Router({ prefix: '/articles' })
 
 router.post('/create', auth, articleInfoValidate, postArticle)
+router.post('/uploads', auth, upload)
 router.post('/restore', auth, verifyAdmin, articleIDValidate, restoreArticle)
 router.delete('/delete', auth, articleIDValidate, articleExistValidate, deleteArticle)
 router.delete('/shield', auth, verifyAdmin, articleIDValidate, articleExistValidate, shieldArticle)
