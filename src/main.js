d737bf575ad11c0a65a0a93d61534a3b8bf9378f
ChenHGitHub <1504784759@qq.com>
2022-03-02 22:54:55 +0800

    完成websocket的整合

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 41
  * @Date: 2022-02-15 15:40:06
- * @LastEditors: 41
- * @LastEditTime: 2022-03-01 16:00:47
+ * @LastEditors: lihao
+ * @LastEditTime: 2022-03-02 22:54:18
  * @Description: 
  */
 
@@ -16,26 +16,64 @@ app.listen(APP_PORT, () => {
   console.log(`server is running on http://localhost:${APP_PORT}`);
 })
 
-//********************************* */
-const WebSocket = require('ws');
-const ws = new WebSocket.Server({ port: 8888 });
-var clients = []
-ws.on('connection', client => {
-  console.log('server connection');
-  if (clients.indexOf(client) === -1) {
-    clients.push(client)
-    console.log("有" + clients.length + "客户端在线");
-    client.on('message', function (msg) {
-      console.log('收到消息' + msg);
-      for (var c of clients) {
-        if (c != client) {
-          c.send(msg)
-        }
+// 已连接的客户端
+let clients = new Array()
+const WebSocket = require('ws')
+// 启动端口为8888
+const wss = new WebSocket.Server({ port: 8888 });
+wss.on('connection', (ws, request) => {
+  console.log(request.url);
+  let id = request.url.substring(1)
+  clients[id + 's'] = ws
+  // 用户连接的提醒
+  console.log('【建立连接】：当前连接用户数量为：' + Object.keys(clients).length);
+  // 监听客户端的消息
+  ws.on('message', (msg) => {
+    console.log(msg)
+    let message = null
+    try {
+      message = JSON.parse(msg)
+    } catch (err) {
+      console.log(err);
+      clients[id + 's'].send(JSON.stringify({
+        fromUId: -1,
+        toUId: id,
+        type: 'error',
+        comtent: '您发送的消息格式有误！'
+      }))
+      return
+    }
+    if (message.type == 'private') {
+      clients[`${message.toUId}s`].send(msg)
+    } else if (message.type == 'group') {
+      for (const key in clients) {
+        clients[key].send(msg)
       }
-    })
-  }
-});
-ws.on('close', client => {
-  clients.splice(clients.indexOf(client), 1)
-  console.log("有" + clients.length + "客户端在线");
+    }
+  })
+  // 监听客户端错误
+  ws.on('error', (err) => {
+    console.log(err);
+  })
+  // 监听客户端断开连接
+  ws.on('close', () => {
+    // 客户端断开连接,删除
+    delete clients[id + 's']
+    console.log('【退出连接】: 当前连接用户数量为：' + Object.keys(data).length);
+  })
+
 })
+// 监听服务关闭
+wss.on('close', () => {
+  console.log('wss close');
+})
+
+
+// const message = {
+//   fromUId: 1,  //  发起通信的用户
+//   toUId: 2,    // 被通信的用户
+//   type: 'private', // 通信的类型， private（私发）、群发（group）、错误（error）
+//   data: { // 传输的数据
+//     reply: 'hello nice to meet you!'
+//   }
+// }
