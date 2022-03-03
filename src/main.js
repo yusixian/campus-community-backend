c002cb94674d76f6f702c68d290b8481cd4ac39b
KamenRider41 <229335832@qq.com>
2022-03-03 14:52:49 +0800

    fix(usersearch): 用户查询取消管理员限制

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 /*
  * @Author: 41
  * @Date: 2022-02-15 15:40:06
-<<<<<<< HEAD
- * @LastEditors: lihao
- * @LastEditTime: 2022-03-02 22:58:26
-=======
- * @LastEditors: cos
- * @LastEditTime: 2022-03-02 11:21:06
->>>>>>> d25ef84081bdbe2e3e5a877e7dcb940c7f6c20c7
+ * @LastEditors: 41
+ * @LastEditTime: 2022-03-03 10:27:39
  * @Description: 
  */
 
