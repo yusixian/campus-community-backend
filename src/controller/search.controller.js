2cc9344d4f2c0e295cecdd7ce188f7a47ff12c3c
KamenRider41 <229335832@qq.com>
2022-03-04 19:13:07 +0800

    feat(usersearch): 用户模糊查询返回增加是否关注信息

--- src/controller/search.controller.js
+++ src/controller/search.controller.js
@@ -2,7 +2,7 @@
  * @Author: 41
  * @Date: 2022-02-24 11:14:28
  * @LastEditors: 41
- * @LastEditTime: 2022-03-04 12:15:37
+ * @LastEditTime: 2022-03-04 19:12:15
  * @Description: 
  */
 const { searchError } = require('../constant/err.type');
@@ -11,19 +11,37 @@ const { selectCommentCountByAid, filterComment } = require('../service/comment.s
 const { filterLike } = require('../service/like.service');
 const { searchLikeUser, searchLikeArticle } = require('../service/search.service')
 const { getUserInfo } = require('../service/user.service')
-
+const { getFollowInfo } = require('../service/follow.service')
 class searchController {
   async searchUser (ctx, next) {
     try {
       // console.log(ctx.request.query);
+      const { id } = ctx.state.user
+      let user_id = id
+      let f_ids = []
+      let follows = await getFollowInfo({ user_id })
+      for (let i = 0; i < follows.length; i++) {
+        // console.log(follows[i].follow_id);
+        f_ids.push(+follows[i].follow_id)
+      }
+      // console.log(f_ids);
+      // console.log(follows);
       let { like_name } = ctx.request.query
-      console.log(like_name);
+      // console.log(like_name);
       let res = await searchLikeUser(like_name)
       let users = []
       for (let i = 0; i < res.length; i++) {
         let { password, ...ans } = res[i]
         users.push(ans)
         // console.log(ans);
+      }
+      for (let i = 0; i < users.length; i++) {
+        // console.log(users[i].id);
+        if (f_ids.indexOf(users[i].id) != -1) {
+          users[i]['follow'] = true
+        } else {
+          users[i]['follow'] = false
+        }
       }
       ctx.body = {
         code: 0,
