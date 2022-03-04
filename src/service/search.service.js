4d0809a61b4852d1038c8c13092804566761c0d2
KamenRider41 <229335832@qq.com>
2022-03-04 12:04:51 +0800

    fix(userlikesearch): 用户模糊查询(模糊昵称和模糊用户名)

--- src/service/search.service.js
+++ src/service/search.service.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 41
  * @Date: 2022-02-24 11:51:17
- * @LastEditors: cos
- * @LastEditTime: 2022-02-28 13:19:52
+ * @LastEditors: 41
+ * @LastEditTime: 2022-03-04 12:02:58
  * @Description: 搜索相关服务
  */
 const User = require('../model/user.model')
@@ -14,9 +14,17 @@ class SearchService {
   async searchLikeUser (clue) {
     return await User.findAll({
       where: {
-        user_name: {
-          [Op.like]: `%${clue}%`
-        }
+        [Op.or]: [
+          {
+            user_name: {
+              [Op.like]: `%${clue}%`
+            },
+          },
+          {
+            name: {
+              [Op.like]: `%${clue}%`
+            }
+          }]
       },
       raw: true
     })
@@ -24,30 +32,30 @@ class SearchService {
   async searchLikeArticle (word, filterOpt) {
     const current = filterOpt.current || 1
     const size = filterOpt.size || 10
-    console.log("filterOpt:",filterOpt)
+    console.log("filterOpt:", filterOpt)
     const { status } = filterOpt
     const whereOpt = getWhereOpt(filterOpt)
-    Object.assign(whereOpt, { 
+    Object.assign(whereOpt, {
       [Op.or]: [
-        { title: { [Op.like]: `%${word || ' '}%`} },
-        { content: {[Op.like]: `${word || ' '}`} },
-        { summary: {[Op.like]: `${word || ' '}`} },
+        { title: { [Op.like]: `%${word || ' '}%` } },
+        { content: { [Op.like]: `${word || ' '}` } },
+        { summary: { [Op.like]: `${word || ' '}` } },
       ]
     })
-    
+
     const paranoidOpt = getParanoidOpt(status)
-    if(!status) whereOpt.status = 0 // 文章搜索默认搜索到已发布
-    console.log("whereOpt:",whereOpt)
-    console.log("paranoidOpt:",paranoidOpt)
+    if (!status) whereOpt.status = 0 // 文章搜索默认搜索到已发布
+    console.log("whereOpt:", whereOpt)
+    console.log("paranoidOpt:", paranoidOpt)
     const { count, rows } = await Article.findAndCountAll({
       where: whereOpt,
       // order: orderOpt,
-      offset: (current-1)*size,
+      offset: (current - 1) * size,
       limit: size,
       paranoid: paranoidOpt,
       raw: true
     })
-    const page_nums = Math.ceil(count/size)
+    const page_nums = Math.ceil(count / size)
     console.log({ page_nums, count, rows })
     return { page_nums, count, rows }
   }
