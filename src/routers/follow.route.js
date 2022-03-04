38110b019f459d92eb7b243ac457d1aabf982a8b
KamenRider41 <229335832@qq.com>
2022-03-04 15:14:09 +0800

    feat(follow): 判断该用户是否关注某位用户

--- src/routers/follow.route.js
+++ src/routers/follow.route.js
@@ -2,7 +2,7 @@
  * @Author: 41
  * @Date: 2022-03-03 16:26:23
  * @LastEditors: 41
- * @LastEditTime: 2022-03-03 17:28:23
+ * @LastEditTime: 2022-03-04 15:04:29
  * @Description: 
  */
 const Router = require('koa-router')
@@ -10,11 +10,12 @@ const Router = require('koa-router')
 const router = new Router({ prefix: '/follows' })
 
 // 导入controller
-const { addfollow, findfollow, delfollow } = require('../controller/follow.controller')
+const { addfollow, findfollow, delfollow, ornot } = require('../controller/follow.controller')
 const { auth } = require('../middleware/auth.middleware')
 const { iffollow } = require('../middleware/follow.middleware')
 // GET 
 router.post('/addfollow', auth, iffollow, addfollow)
 router.get('/findfollow', findfollow)
 router.post('/delfollow', auth, delfollow)
+router.get('/ornot', auth, ornot)
 module.exports = router
