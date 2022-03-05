dad26cbc956e1b7bd34d3c1c07e01f9bb9d27e26
cos <3450414733@qq.com>
2022-03-05 21:49:52 +0800

    refactor(seq.js): 更改sequelize日志输出文件，现在会输出到logs/seq.log中

--- src/db/seq.js
+++ src/db/seq.js
@@ -1,11 +1,14 @@
 /*
  * @Author: 41
  * @Date: 2022-02-16 00:04:26
- * @LastEditors: lihao
- * @LastEditTime: 2022-03-04 17:00:06
+ * @LastEditors: cos
+ * @LastEditTime: 2022-03-05 21:46:27
  * @Description: 
  */
 const { Sequelize } = require('sequelize')
+const fs = require('fs')
+const path = require('path')
+
 const {
   MYSQL_HOST,
   MYSQL_PORT,
@@ -13,12 +16,21 @@ const {
   MYSQL_PWD,
   MYSQL_DB } = require('../config/config.default')
 
-
+const myLogFunc = function(msg) {
+  try {
+    const logPath = path.join(__dirname, '../../logs/seq.log')
+    const data = fs.writeFileSync(logPath, '\n'+msg, { flag: 'a+' })
+    //文件写入成功。
+  } catch (err) {
+    console.error(err)
+  }
+}
 const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
   host: MYSQL_HOST,
   dialect: 'mysql',
   port: MYSQL_PORT,
-  timezone: '+08:00'
+  timezone: '+08:00',
+  logging: myLogFunc
 })
 
 // seq.authenticate().then(() => {
