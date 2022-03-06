adb9e026c516ec2ff53c51a7d2fad424d222a2fa
ChenHGitHub <1504784759@qq.com>
2022-03-06 11:57:19 +0800

    完成日志传输和关闭接口

--- src/ws/service/ws.service.js
+++ src/ws/service/ws.service.js
@@ -1,15 +1,25 @@
 /*
  * @Author: lihao
  * @Date: 2022-03-03 19:53:23
- * @LastEditors: cos
- * @LastEditTime: 2022-03-05 20:44:57
+ * @LastEditors: lihao
+ * @LastEditTime: 2022-03-06 11:55:47
  * @FilePath: \campus-community-backend\src\ws\service\ws.service.js
  * @Description: 
  * 
  * Copyright (c) 2022 by 用户/公司名, All Rights Reserved. 
  */
 let { ws } = require('../wsClient')
-let {clients} = require('../wsServer')
+let { clients } = require('../wsServer')
+
+const fs = require('fs');
+const path = require('path');
+
+let seqWatcher = null
+let oldSeqContent = ""
+let errWatcher = null
+let oldErrContent = ""
+let outWathcher = null
+let oldOutContent = ""
 class WsService {
   /**
    * 后端私发消息
@@ -45,7 +55,6 @@ class WsService {
         type: 'group', // 通信的类型， private（私发）、群发（group）、错误（error）
         data: data
       }
-      console.log(message);
       ws.send(JSON.stringify(message))
       return true
     } catch (err) {
@@ -57,17 +66,201 @@ class WsService {
    * 获取所有在线用户列表和数量
    * @returns 
    */
-  async getAllOnlineUser(){
+  async getAllOnlineUser() {
     let onlineUserIds = []
     for (const key in clients) {
-      onlineUserIds.push(Number.parseInt(key.substring(0, key.length-1)))
+      onlineUserIds.push(Number.parseInt(key.substring(0, key.length - 1)))
     }
     return {
       onlineUserIds,
       count: onlineUserIds.length
     }
   }
+  /**
+   * 传输seq.log
+   * @param {*} filepath 文件路径
+   * @param {*} wsClients  需要传输的客户端
+   */
+  async sendSeqLogContent(filepath, wsClients) {
+    try {
+      // 获取日志所在路径
+      let fullPath = path.join(__dirname, filepath)
+      // 获取初始日志文件
+      if (oldSeqContent == "") {
+        fs.readFile(fullPath, 'utf-8', (err, data) => {
+          if (err) {
+            return false
+          }
+          oldSeqContent = data
+        })
+      }
+      // 创建seq初始监听器
+      if (seqWatcher == null) {
+        seqWatcher = fs.watch(fullPath, (event, fiename) => {
+          if (event == "change") { // 当为change时间时，进行传输
+            fs.readFile(fullPath, 'utf-8', (err, data) => {
+              if (err) {
+                return false
+              }
+              // if (data == oldSeqContent) {
+              //   return
+              // }
+              let newSeqLog = data.replace(oldSeqContent, "")
+              for (let i = 0; i < wsClients.length; i++) {
+                if (clients[wsClients[i] + 's']) {
+                  clients[wsClients[i] + 's'].send(JSON.stringify({
+                    fromUId: 0,
+                    toUId: wsClients[i],
+                    type: 'seqlog',
+                    data: newSeqLog
+                  }))
+                }
 
+              }
+            })
+          }
+        })
+      }
+      return true
+
+    } catch (err) {
+      console.log(err);
+      return false
+
+    }
+
+  }
+  /**
+   * 传输错误日志
+   * @param {*} filepath 文件路径
+   * @param {*} wsClients 需要传输的客户端
+   */
+  async sendErrLogContent(filepath, wsClients) {
+    try {
+      // 获取日志所在路径
+      let fullPath = path.join(__dirname, filepath)
+      // 获取初始日志文件
+      if (oldErrContent == "") {
+        fs.readFile(fullPath, 'utf-8', (err, data) => {
+          if (err) {
+            return false
+          }
+          oldErrContent = data
+        })
+      }
+      // 创建seq初始监听器
+      if (errWatcher == null) {
+        errWatcher = fs.watch(fullPath, (event, fiename) => {
+          if (event == "change") { // 当为change时间时，进行传输
+            fs.readFile(fullPath, 'utf-8', (err, data) => {
+              if (err) {
+                return false
+              }
+              // if (data == oldErrContent) {
+              //   return
+              // }
+              let newErrLog = data.replace(oldErrContent, "")
+              for (let i = 0; i < wsClients.length; i++) {
+                if (clients[wsClients[i] + 's']) {
+                  clients[wsClients[i] + 's'].send(JSON.stringify({
+                    fromUId: 0,
+                    toUId: wsClients[i],
+                    type: 'seqlog',
+                    data: newErrLog
+                  }))
+                }
+              }
+            })
+          }
+        })
+      }
+      return true
+
+    } catch (err) {
+      console.log(err);
+      return false
+
+    }
+
+  }
+  /**
+   * 传输输出日志
+   * @param {*} filepath 文件路径
+   * @param {*} wsClients 需要传输的客户端
+   */
+  async sendOutLogContent(filepath, wsClients) {
+    try {
+      // 获取日志所在路径
+      let fullPath = path.join(__dirname, filepath)
+      // 获取初始日志文件
+      if (oldOutContent == "") {
+        fs.readFile(fullPath, 'utf-8', (err, data) => {
+          if (err) {
+            return false
+          }
+          oldOutContent = data
+        })
+      }
+      // 创建seq初始监听器
+      if (outWathcher == null) {
+        outWathcher = fs.watch(fullPath, (event, fiename) => {
+          if (event == "change") { // 当为change时间时，进行传输
+            fs.readFile(fullPath, 'utf-8', (err, data) => {
+              if (err) {
+                return false
+              }
+              // if (data == oldOutContent) {
+              //   return
+              // }
+              let newErrLog = data.replace(oldOutContent, "")
+              for (let i = 0; i < wsClients.length; i++) {
+                if (clients[wsClients[i] + 's']) {
+                  clients[wsClients[i] + 's'].send(JSON.stringify({
+                    fromUId: 0,
+                    toUId: wsClients[i],
+                    type: 'seqlog',
+                    data: newErrLog
+                  }))
+                }
+              }
+            })
+          }
+        })
+      }
+      return true
+    } catch (err) {
+      console.log(err);
+      return false
+
+    }
+
+  }
+  /**
+   * 
+   * @param {*} type 需要关闭的类型
+   * @param {*} clients 现存的客户端数量
+   * @returns 
+   */
+  async closeWatcher(type) {
+    try{
+      if (type == 'seq') {
+        seqWatcher.close()
+        console.log(seqWatcher);
+        seqWatcher = null
+      } else if (type == 'err') {
+        errWatcher.close()
+        errWatcher = null
+      } else if (type == 'out') {
+        outWathcher.close()
+        outWathcher = null
+      }
+      return true
+    }catch(err) {
+      console.log(err);
+      return false
+    }
+    
+  }
 
 }
 
--- src/ws/wsController/ws.controller.js
+++ src/ws/wsController/ws.controller.js
@@ -1,15 +1,18 @@
 /*
  * @Author: lihao
  * @Date: 2022-03-03 20:12:41
- * @LastEditors: cos
- * @LastEditTime: 2022-03-05 20:45:13
+ * @LastEditors: lihao
+ * @LastEditTime: 2022-03-06 11:50:50
  * @FilePath: \campus-community-backend\src\ws\wsController\ws.controller.js
  * @Description: 
  * 
  * Copyright (c) 2022 by 用户/公司名, All Rights Reserved. 
  */
-const { getAllOnlineUser, sendPrivateMessage, sendGroupMessage } = require('../service/ws.service')
-const {wsGetOnlineUserError, wsSendMessageError} = require('../../constant/err.type')
+const { getAllOnlineUser, sendPrivateMessage, sendGroupMessage, sendSeqLogContent, sendErrLogContent, sendOutLogContent, closeWatcher } = require('../service/ws.service')
+const { wsGetOnlineUserError, wsSendMessageError } = require('../../constant/err.type')
+let wsSeqLogClients = []
+let wsErrLogClients = []
+let wsOutLogClients = []
 class wsController {
   /**
    * 查询在线用户人数的接口
@@ -26,9 +29,9 @@ class wsController {
           res
         }
       }
-    }catch(err) {
+    } catch (err) {
       console.error(err);
-      ctx.app.emit('error', wsGetOnlineUserError,ctx)
+      ctx.app.emit('error', wsGetOnlineUserError, ctx)
     }
   }
   /**
@@ -37,22 +40,22 @@ class wsController {
    * @param {*} next 
    */
   async sendMessageToUid(ctx, next) {
-    const {toUId, data} = ctx.request.body
+    const { toUId, data } = ctx.request.body
     try {
-      if(await sendPrivateMessage(toUId, data)){
+      if (await sendPrivateMessage(toUId, data)) {
         ctx.body = {
           code: 0,
           message: '发送消息成功'
         }
-      }else {
+      } else {
         ctx.body = {
           code: 1,
           message: '发送消息失败'
         }
       }
-    }catch(err) {
+    } catch (err) {
       console.error(err);
-      ctx.app.emit('error', wsSendMessageError,ctx)
+      ctx.app.emit('error', wsSendMessageError, ctx)
     }
   }
   /**
@@ -61,25 +64,111 @@ class wsController {
    * @param {*} next 
    */
   async sendMessageToAllOnline(ctx, next) {
-    const {data} = ctx.request.body
+    const { data } = ctx.request.body
     console.log(data);
     try {
-      if(await sendGroupMessage(data)){
+      if (await sendGroupMessage(data)) {
         ctx.body = {
           code: 0,
           message: '发送消息成功'
         }
-      }else {
+      } else {
         ctx.body = {
           code: 1,
           message: '发送消息失败'
         }
       }
-    }catch(err) {
+    } catch (err) {
       console.error(err);
-      ctx.app.emit('error', wsSendMessageError,ctx)
+      ctx.app.emit('error', wsSendMessageError, ctx)
     }
   }
+  /**
+   * 开始监听日志
+   * @param {*} ctx 
+   * @param {*} next 
+   */
+  async beginSendLogContent(ctx, next) {
+    let isSuccess = false
+    try {
+      const {type } = ctx.request.body
+      let id = ctx.state.user.id
+      if (type == "seq") {
+        if (wsSeqLogClients.indexOf(id) == -1) {
+          wsSeqLogClients.push(id)
+        }
+        isSuccess = await sendSeqLogContent('../../../logs/seq.log', wsSeqLogClients)
+      } else if (type == "err") {
+        if (wsErrLogClients.indexOf(id) == -1) {
+          wsErrLogClients.push(id)
+        }
+        isSuccess = await sendErrLogContent('../../../logs/err.log', wsErrLogClients)
+      } else if (type == "out") {
+        if (wsOutLogClients.indexOf(id) == -1) {
+          wsOutLogClients.push(id)
+        }
+        isSuccess = await sendOutLogContent('../../../logs/err.log', wsOutLogClients)
+      }
+      if (isSuccess) {
+        ctx.body = {
+          code: 0,
+          message: "开始监听"
+        }
+      } else {
+        ctx.body = {
+          code: 0,
+          message: "监听失败"
+        }
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  /**
+   * 关闭传送
+   * @param {*} ctx 
+   * @param {*} next 
+   */
+  async endSendLog(ctx, next) {
+    let {type} = ctx.request.body
+    let id = ctx.state.user.id
+    let isSuccess = true
+    try{
+      if (type == 'seq') {
+        wsSeqLogClients.splice(wsSeqLogClients.indexOf(id), 1)
+        if(wsSeqLogClients.length == 0) {
+          isSuccess = closeWatcher(type)
+        }
+      } else if (type == 'err') {
+        wsErrLogClients.splice(wsErrLogClients.indexOf(id), 1)
+        if(wsErrLogClients.length == 0) {
+          isSuccess = closeWatcher(type)
+        }
+      } else if (type == 'out') {
+        wsOutLogClients.splice(wsOutLogClients.indexOf(id), 1)
+        if(wsOutLogClients.length == 0) {
+          isSuccess = closeWatcher(type)
+        }
+      }
+      if (isSuccess) {
+        ctx.body = {
+          code: 0,
+          message: "传输停止成功"
+        }
+      }else {
+        ctx.body = {
+          code: 0,
+          message: "传输停止失败"
+        }
+      }
+      
+    }catch(err) {
+      console.log(err);
+    }
+  }
+
 }
 
 module.exports = new wsController()
